4a570b87e6c768fdac5f29474bda1453c0ed4590
Vaishali-962 <vaishaliwankhade69200@gmail.com>
2026-04-26 22:50:01 +0530

    initialize shopnest-platform parent project

---
+++ src/main/java/com/ecom/shopnest/ShopnestPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.ecom.shopnest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopnestPlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShopnestPlatformApplication.class, args);
+	}
+
+}
