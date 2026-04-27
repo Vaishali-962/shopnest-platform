98c72c2ef94180f5f2a2ba207af7cabf2b04e960
Vaishali-962 <vaishaliwankhade69200@gmail.com>
2026-04-27 09:30:14 +0530

    Added eureka service, remove src from parent module

--- eureka-server/src/test/java/com/ecom/eurekaserver/EurekaServerApplicationTests.java
+++ eureka-server/src/test/java/com/ecom/eurekaserver/EurekaServerApplicationTests.java
@@ -1,13 +1,13 @@
-package com.ecom.shopnest;
+package com.ecom.eurekaserver;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class ShopnestPlatformApplicationTests {
+class EurekaServerApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
